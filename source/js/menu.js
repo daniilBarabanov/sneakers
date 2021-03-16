71658931fd65f993a21bdeebb02f3cf267a69b05
daniilBarabanov <55635367+daniilBarabanov@users.noreply.github.com>
2021-03-16 16:44:39 +0300

    bergerv2

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
     $('.page-header__burger').click(function(){
         $('.page-header__burger').toggleClass('open-menu');
         $('.page-header__nav').toggleClass('open-menu');
+        $('body').toggleClass('fixed-page');
     });
 });
