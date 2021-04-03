64ba39ac5001054674d31a002c69eae4cb876229
daniilBarabanov <flame146@mail.ru>
2021-04-03 23:44:31 +0300

    part2

--- source/js/carousel.js
+++ source/js/carousel.js
@@ -1,19 +1,51 @@
 'use strict';
 $(document).ready(function() {
     let position = 0;
-    const slidesToShow = 3;
+    const slidesToShow = 3; // Количество видимых слайдов
     const slidesToScroll = 2;
-    const container = $('.slider-container');
-    const track = $('.slider-track');
-    const item = $('.slider-item');
-    const btnPrev = $('.btn-prev'); 
-    const btnNext = $('.btn-next'); 
-    const itemWidth = container.width() / slidesToShow;
+    const container = $('.slider-container'); // Находим общий контейнер карусели
+    const track = $('.slider-track'); // Находим сам трек карусели, который будем двигать
+    const item = $('.slider-item'); // Находим элементы карусели
+    const btnPrev = $('.btn-prev'); // Кнопка прев
+    const btnNext = $('.btn-next'); // Кнопка некст
+    const itemsCount = item.length; // Суммарное кол-во элементов
+    const itemWidth = container.width() / slidesToShow; // Ширина каждого элемента в карусели
+    const movePosition = slidesToScroll * itemWidth; // Определяем дистанцию, на которую нам надо проскроллить, то есть умножаем кол-во элементов, которые надо проскроллить на их ширину
 
+    
     item.each(function(index, item) {
         $(item).css({
             minWidth: itemWidth,
         });
     });
+
+    btnNext.click(function(){
+        const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+        position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+
+        setPosition();
+        checkBtns();
+    });
+    btnPrev.click(function(){
+        const itemsLeft = Math.abs(position) / itemWidth;
+        position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+
+        setPosition();
+        checkBtns();
+    });
+    const setPosition = () => {
+        track.css({
+            transform: `translateX(${position}px)`
+        });
+    };
+
+    const checkBtns = () => {
+        btnPrev.prop('disabled',position >= 0);
+        btnNext.prop(
+            'disabled',
+            position <= -(itemsCount - slidesToShow) * itemWidth,
+         );
+    };
+    checkBtns();
 });
-console.log('kkk');
+
